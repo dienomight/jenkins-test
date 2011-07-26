f291ce1e4419550ef9a21df67c2aef16d360f99c
dienomight <dienomight@o2.pl>
2011-07-26 16:26:30 +0200

    Increasing number of duplicated lines for phpcpd to notice duplicates

--- DemoCopyPaste.php
+++ DemoCopyPaste.php
@@ -3,11 +3,19 @@
 class DemoCopyPaste {
     
     function foo() {
+	$lets = 'create'
+	$more = 'duplicated';
+	$lines = 'to make';
+	$phpcpd = 'angry';
 	$str = 'bar';	
         return str;
     }
 
     function foo2() {
+	$lets = 'create'
+	$more = 'duplicated';
+	$lines = 'to make';
+	$phpcpd = 'angry';
 	$str = 'bar';	
         return str;
     }
